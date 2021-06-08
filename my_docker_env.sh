a767d622ad4adf1f37f7400daf5d4b8cc2b70532
Ty Nguyen <t.nguyen5@partner.samsung.com>
2021-06-07 20:40:45 -0400

    Update my_docker_env.sh

--- my_docker_env.sh
+++ my_docker_env.sh
@@ -1,5 +1,5 @@
 # Container we want to build 
 user_name=$(whoami)
 export DOCKER_IMG_NAME="tynguyen/saicny_ubuntu18.04"
-export DOCKER_IMG_TAG="dev-cuda10.2-cudnn8"
+export DOCKER_IMG_TAG="kinect-cuda10.2-cudnn8"
 export CONTAINER_NAME="${user_name}_docker"  
